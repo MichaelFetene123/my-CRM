9579926d79b5d2992eea9d21c015597546437dd0
MichaelFetene123 <michaelfetene0202@gmail.com>
2026-08-10 09:49:14 +0300

    feat: initialize CRM domain models, morph map configuration, and pipeline stage seeding

---
+++ database/seeders/PipelineStageSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PipelineStageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */// database/seeders/PipelineStageSeeder.php
+public function run(): void
+{
+    $stages = [
+        ['name' => 'New', 'order' => 1, 'is_won' => false, 'is_lost' => false],
+        ['name' => 'Contacted', 'order' => 2, 'is_won' => false, 'is_lost' => false],
+        ['name' => 'Proposal Sent', 'order' => 3, 'is_won' => false, 'is_lost' => false],
+        ['name' => 'Negotiation', 'order' => 4, 'is_won' => false, 'is_lost' => false],
+        ['name' => 'Won', 'order' => 5, 'is_won' => true, 'is_lost' => false],
+        ['name' => 'Lost', 'order' => 6, 'is_won' => false, 'is_lost' => true],
+    ];
+
+    foreach ($stages as $stage) {
+        \App\Models\PipelineStage::create($stage);
+    }
+}
+}
